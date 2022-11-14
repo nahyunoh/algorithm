d892b03690aec3c1aa45cfe8f2c3e619d7ab77a6
nahyunoh <onheh28@gmail.com>
2022-11-14 23:57:45 +0900

    단계별로 풀어보기 - 함수

---
+++ function/func_sum_15596.java
@@ -0,0 +1,15 @@
+package function;
+
+public class func_sum_15596 {
+
+    long sum(int[] a){
+        long sum=0;
+
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+
+        return sum;
+    }
+    
+}
